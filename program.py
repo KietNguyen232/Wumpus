3cfca78abfab129c0605b29bf77cbfb9da8d706f
Nguyễn Anh Kiệt <nakiet22@clc.fitus.edu.vn>
2024-08-05 19:23:10 +0700

    Update program.py
    Update the logical error that Stench, Glow are deleted even there are still Wumpus and Healing Potion nearby.

--- program.py
+++ program.py
@@ -11,26 +11,29 @@ class Program:
                         temp[-1] = temp[-1][:-1]
                     for j in range(self.size):
                         self.map[i].append(temp[j].split(" "))
-                for i in range(self.size):
-                    for j in range(self.size):
-                        if "A" in self.map[i][j]:
-                            self.map[i][j].pop(self.map[i][j].index("A"))
-                            self.map[i][j].append("-")
-                            self.agentLocation = (i, j)
-                        if "W" in self.map[i][j]:
-                            self.assignValue("S", i, j)
-                        if "P" in self.map[i][j]:
-                            self.assignValue("B", i, j)
-                        if "P_G" in self.map[i][j]:
-                            self.assignValue("W_H", i, j)
-                        if "H_P" in self.map[i][j]:
-                            self.assignValue("G_L", i, j)
-                for i in range(self.size):
-                    for j in range(self.size):
-                        if len(self.map[i][j]) > 1 and "-" in self.map[i][j]:
-                            self.map[i][j].pop(self.map[i][j].index("-"))
+                self.updateMap()
         except:
             raise Exception("Invalid input file or file format!")
+    
+    def updateMap(self):
+        for i in range(self.size):
+            for j in range(self.size):
+                if "A" in self.map[i][j]:
+                    self.map[i][j].pop(self.map[i][j].index("A"))
+                    self.map[i][j].append("-")
+                    self.agentLocation = (i, j)
+                if "W" in self.map[i][j]:
+                    self.assignValue("S", i, j)
+                if "P" in self.map[i][j]:
+                    self.assignValue("B", i, j)
+                if "P_G" in self.map[i][j]:
+                    self.assignValue("W_H", i, j)
+                if "H_P" in self.map[i][j]:
+                    self.assignValue("G_L", i, j)
+        for i in range(self.size):
+            for j in range(self.size):
+                if len(self.map[i][j]) > 1 and "-" in self.map[i][j]:
+                    self.map[i][j].pop(self.map[i][j].index("-"))
     
     def assignValue(self, value, row, col):
         if row + 1 < self.size and value not in self.map[row + 1][col]:
@@ -48,6 +51,7 @@ class Program:
     def AgentShoot(self, cell):
         row, col = cell
         deleteCell = []
+        temp = []
         if "W" in self.map[row][col]:
             self.map[row][col].pop(self.map[row][col].index("W"))
             if len(self.map[row][col]) == 0:
@@ -56,22 +60,26 @@ class Program:
                 self.map[row + 1][col].pop(self.map[row + 1][col].index("S"))
                 if len(self.map[row + 1][col]) == 0:
                     self.map[row + 1][col].append("-")
-                deleteCell.append((row + 1, col))
+                temp.append((row + 1, col))
             if col + 1 < self.size:
                 self.map[row][col + 1].pop(self.map[row][col + 1].index("S"))
                 if len(self.map[row][col + 1]) == 0:
                     self.map[row][col + 1].append("-")
-                deleteCell.append((row, col + 1))
+                temp.append((row, col + 1))
             if row - 1 >= 0:
                 self.map[row - 1][col].pop(self.map[row - 1][col].index("S"))
                 if len(self.map[row - 1][col]) == 0:
                     self.map[row - 1][col].append("-")
-                deleteCell.append((row - 1, col))
+                temp.append((row - 1, col))
             if col - 1 >= 0:
                 self.map[row][col - 1].pop(self.map[row][col - 1].index("S"))
                 if len(self.map[row][col - 1]) == 0:
                     self.map[row][col - 1].append("-")
-                deleteCell.append((row, col - 1))
+                temp.append((row, col - 1))
+            self.updateMap()
+            for cell in temp:
+                if "S" not in self.map[cell[0]][cell[1]]: 
+                    deleteCell.append(cell)
             return "SCREAM", deleteCell
         return "", deleteCell
       
@@ -86,6 +94,7 @@ class Program:
     def agentGrabHP(self, cell):
         row, col = cell
         deleteCell = []
+        temp = []
         if "H_P" in self.map[row][col]:
             self.map[row][col].pop(self.map[row][col].index("H_P"))
             if len(self.map[row][col]) == 0:
@@ -94,22 +103,26 @@ class Program:
                 self.map[row + 1][col].pop(self.map[row + 1][col].index("G_L"))
                 if len(self.map[row + 1][col]) == 0:
                     self.map[row + 1][col].append("-")
-                deleteCell.append((row + 1, col))
+                temp.append((row + 1, col))
             if col + 1 < self.size:
                 self.map[row][col + 1].pop(self.map[row][col + 1].index("G_L"))
                 if len(self.map[row][col + 1]) == 0:
                     self.map[row][col + 1].append("-")
-                deleteCell.append((row, col + 1))
+                temp.append((row, col + 1))
             if row - 1 >= 0:
                 self.map[row - 1][col].pop(self.map[row - 1][col].index("G_L"))
                 if len(self.map[row - 1][col]) == 0:
                     self.map[row - 1][col].append("-")
-                deleteCell.append((row - 1, col))
+                temp.append((row - 1, col))
             if col - 1 >= 0:
                 self.map[row][col - 1].pop(self.map[row][col - 1].index("G_L"))
                 if len(self.map[row][col - 1]) == 0:
                     self.map[row][col - 1].append("-")
-                deleteCell.append((row, col - 1))
+                temp.append((row, col - 1))
+            self.updateMap()
+            for cell in temp:
+                if "G_L" not in self.map[cell[0]][cell[1]]: 
+                    deleteCell.append(cell)
             return deleteCell
         return deleteCell
     
