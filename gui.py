fb3c0de10a97843a0ef53c671c25a5ae524ab302
22127252VoVietLong <vvlong22@clc.fitus.edu.vn>
2024-08-18 23:14:27 +0700

    Gui with review map

--- gui.py
+++ gui.py
@@ -2,20 +2,17 @@ import pygame as pg
 import program
 import sys
 import agent 
-
+pg.font.init()
+pg.display.init()
 class GUI:
     def __init__(self, map = "map1.txt") -> None:
         self.agent = agent.Agent(map)
         self.mapSize = self.agent.WumpusWorld.size
+        self.map = self.agent.WumpusWorld.map
         self.screenW =  1300
         self.screenH = 1000
         self.screen = pg.display.set_mode((self.screenW, self.screenH))
-        self.statusCell = (0,0,400, 200) #left, top, width, height        self.currentCell = [0,0,400, 200]
-        self.currentCell = (0, 200, 400, 200)
-        self.topCell = (0,400,400, 200)
-        self.leftCell = (0,600,400, 200)
-        self.rightCell = (400,800,400, 200)
-        self.bottomCell = (800,800,400, 200)
+        self.statusCell = (25,100,255,700) #left, top, width, height        self.currentCell = [0,0,400, 200]
         self.board = (300, 0, 1000, 1000)
         self.cellW = 100
         self.cellH = 100
@@ -35,11 +32,12 @@ class GUI:
         self._whiff = pg.transform.scale(pg.image.load(".\img\whiff.png"),(100, 100))
         self._nothing = pg.transform.scale(pg.image.load(".\img\\nothing.png"), (96, 96))
         self._hud = pg.image.load(".\img\\newhud.png")
+        self._review = pg.image.load(".\img\\reviewhud.png")
+
     def getAgentPicture(self, direction:tuple):
         match direction:
             case (-1, 0):
                 return self._agent[0]
-
             case (0, 1):
                 return self._agent[1]
             case (1, 0):
@@ -74,100 +72,175 @@ class GUI:
             case "G_L": 
                 return self._glow
         return self._nothing
-    def getPerceptCell(self, direction:str):
-        match direction.lower():
-            case "left":
-                return self.leftCell
-            case "right":
-                return self.rightCell
-            case "top":
-                return self.topCell
-            case "bottom":
-                return self.bottomCell
-        return (0,0,0,0)
+    def showStatus(self, score, agentHP, gold, potion, action):
+        offsetX, offsetY, width, height = self.statusCell
+        posX = offsetX + 16
+        posY = offsetY + 100
+        eraser = pg.Rect(0,0,   width - 25, 34 )
+        font = pg.font.Font("COMIC.TTF", 24)
+        healthText = font.render(f"Health: {str(agentHP)}", True, (0,0,0))
+        eraser.topleft = (posX, posY)
+        pg.draw.rect(self.screen, (160,160,160), eraser)
+        self.screen.blit(healthText, (posX, posY))
+        posY += healthText.get_rect().size[1] + 2
+
+        scoreText = font.render(f"Score: {str(score)}", True, (0,0,0))
+        eraser.topleft = (posX, posY)
+        pg.draw.rect(self.screen, (160,160,160), eraser)
+        self.screen.blit(scoreText, (posX, posY))
+        posY += scoreText.get_rect().size[1] + 2
+
+        goldText = font.render(f"Gold: {str(gold)}", True, (0,0,0))
+        eraser.topleft = (posX, posY)
+        pg.draw.rect(self.screen, (160,160,160), eraser)
+        self.screen.blit(goldText, (posX, posY))
+        posY += goldText.get_rect().size[1] + 2
+
+        potionText = font.render(f"Potion: {str(potion)}", True, (0,0,0))
+        eraser.topleft = (posX, posY)
+        pg.draw.rect(self.screen, (160,160,160), eraser)
+        self.screen.blit(potionText, (posX, posY))
+        posY += potionText.get_rect().size[1] + 2
+
+        temp = str(action)
+        if (temp == "SCREAM"):
+            temp = "Hear scream"
+        actionText = font.render(f"Action: {str(temp)}", True, (0,0,0))
+        eraser.topleft = (posX, posY)
+        pg.draw.rect(self.screen, (160,160,160), eraser)
+        self.screen.blit(actionText, (posX, posY))
+        posY += actionText.get_rect().size[1] + 2
+
+        pg.display.flip()
+    def drawItemInCell(self, location, items):
+        posx = self.board[0] + location[1]*(self.cellW) + (self.cellW - self._nothing.get_rect().size[0])/2
+        posy = self.board[1] + (location[0])*(self.cellH) + (self.cellH - self._nothing.get_rect().size[1])/2
+        self.screen.blit(self._nothing, (posx, posy))
+        for i in range(len(items)):
+            img = self.getProperpyImage(items[i])
+            posx = self.board[0] + location[1]*(self.cellW) + (self.cellW - img.get_rect().size[0])/2
+            posy = self.board[1] + (location[0])*(self.cellH) + (self.cellH - img.get_rect().size[1])/2
+            self.screen.blit(img, (posx, posy))
+    def drawReviewMap(self):
+        for i in range(self.mapSize):
+            for j in range(self.mapSize):
+                # self.screen.blit(self._nothing, (i, j))
+                items = self.map[i][j]
+                self.drawItemInCell((i, j), items)
+        pg.display.flip()
     def drawPerceptInfo(self, location:tuple, percepts:list):
         if (location[0] -  1 >= 0):
             if (percepts[location[0] -  1][location[1]]):
-                posx = self.board[0] + location[1]*(self.cellW) + (self.cellW - self._nothing.get_rect().size[0])/2
-                posy = self.board[1] + (location[0] - 1)*(self.cellH) + (self.cellH - self._nothing.get_rect().size[1])/2
-                self.screen.blit(self._nothing, (posx, posy))
+                # posx = self.board[0] + location[1]*(self.cellW) + (self.cellW - self._nothing.get_rect().size[0])/2
+                # posy = self.board[1] + (location[0] - 1)*(self.cellH) + (self.cellH - self._nothing.get_rect().size[1])/2
+                # self.screen.blit(self._nothing, (posx, posy))
                 percept = percepts[location[0] -  1][location[1]]
-                for i in range(len(percept)):
-                    img = self.getProperpyImage(percept[i])
-                    posx = self.board[0] + location[1]*(self.cellW) + (self.cellW - img.get_rect().size[0])/2
-                    posy = self.board[1] + (location[0] - 1)*(self.cellH) + (self.cellH - img.get_rect().size[1])/2
-                    self.screen.blit(img, (posx, posy))
+                self.drawItemInCell((location[0] -  1, location[1]), percept)
+                # for i in range(len(percept)):
+                #     img = self.getProperpyImage(percept[i])
+                #     posx = self.board[0] + location[1]*(self.cellW) + (self.cellW - img.get_rect().size[0])/2
+                #     posy = self.board[1] + (location[0] - 1)*(self.cellH) + (self.cellH - img.get_rect().size[1])/2
+                #     self.screen.blit(img, (posx, posy))
         if (location[0] +  1 < self.mapSize):
             if (percepts[location[0] +  1][location[1]]):
                 percept = percepts[location[0] +  1][location[1]]
-                posx = self.board[0] + location[1]*(self.cellW) + (self.cellW - self._nothing.get_rect().size[0])/2
-                posy = self.board[1] + (location[0] + 1)*(self.cellH) + (self.cellH - self._nothing.get_rect().size[1])/2
-                self.screen.blit(self._nothing, (posx, posy))
-                for i in range(len(percept)):
-                    img = self.getProperpyImage(percept[i])
-                    posx = self.board[0] + location[1]*(self.cellW) + (self.cellW - img.get_rect().size[0])/2
-                    posy = self.board[1] + (location[0] + 1)*(self.cellH) + (self.cellH - img.get_rect().size[1])/2
-                    self.screen.blit(img, (posx, posy))
+                # posx = self.board[0] + location[1]*(self.cellW) + (self.cellW - self._nothing.get_rect().size[0])/2
+                # posy = self.board[1] + (location[0] + 1)*(self.cellH) + (self.cellH - self._nothing.get_rect().size[1])/2
+                # self.screen.blit(self._nothing, (posx, posy))
+                self.drawItemInCell((location[0] +  1, location[1]), percept)
+
+                # for i in range(len(percept)):
+                #     img = self.getProperpyImage(percept[i])
+                #     posx = self.board[0] + location[1]*(self.cellW) + (self.cellW - img.get_rect().size[0])/2
+                #     posy = self.board[1] + (location[0] + 1)*(self.cellH) + (self.cellH - img.get_rect().size[1])/2
+                #     self.screen.blit(img, (posx, posy))
         if (location[1] -  1 >= 0):
             if (percepts[location[0]][location[1] - 1]):
                 percept = percepts[location[0]][location[1] - 1]
-                posx = self.board[0] + (location[1] - 1)*(self.cellW) + (self.cellW - self._nothing.get_rect().size[0])/2
-                posy = self.board[1] + (location[0])*(self.cellH) + (self.cellH - self._nothing.get_rect().size[1])/2
-                self.screen.blit(self._nothing, (posx, posy))
-                for i in range(len(percept)):
-                    img = self.getProperpyImage(percept[i])
-                    posx = self.board[0] + (location[1] - 1)*(self.cellW) + (self.cellW - img.get_rect().size[0])/2
-                    posy = self.board[1] + location[0]*(self.cellH) + (self.cellH - img.get_rect().size[1])/2
-                    self.screen.blit(img, (posx, posy))
+                # posx = self.board[0] + (location[1] - 1)*(self.cellW) + (self.cellW - self._nothing.get_rect().size[0])/2
+                # posy = self.board[1] + (location[0])*(self.cellH) + (self.cellH - self._nothing.get_rect().size[1])/2
+                # self.screen.blit(self._nothing, (posx, posy))
+                self.drawItemInCell((location[0], location[1] - 1), percept)
+                # for i in range(len(percept)):
+                #     img = self.getProperpyImage(percept[i])
+                #     posx = self.board[0] + (location[1] - 1)*(self.cellW) + (self.cellW - img.get_rect().size[0])/2
+                #     posy = self.board[1] + location[0]*(self.cellH) + (self.cellH - img.get_rect().size[1])/2
+                #     self.screen.blit(img, (posx, posy))
         if (location[1] +  1 < self.mapSize):
             if (percepts[location[0]][location[1] + 1]):
                 percept = percepts[location[0]][location[1] + 1]
-                posx = self.board[0] + (location[1] + 1)*(self.cellW) + (self.cellW - self._nothing.get_rect().size[0])/2
-                posy = self.board[1] + (location[0])*(self.cellH) + (self.cellH - self._nothing.get_rect().size[1])/2
-                self.screen.blit(self._nothing, (posx, posy))
-                for i in range(len(percept)):
-                    img = self.getProperpyImage(percept[i])
-                    posx = self.board[0] + (location[1] + 1)*(self.cellW) + (self.cellW - img.get_rect().size[0])/2
-                    posy = self.board[1] + location[0]*(self.cellH) + (self.cellH - img.get_rect().size[1])/2
-                    self.screen.blit(img, (posx, posy))
-        pg.display.flip()
+                # posx = self.board[0] + (location[1] + 1)*(self.cellW) + (self.cellW - self._nothing.get_rect().size[0])/2
+                # posy = self.board[1] + (location[0])*(self.cellH) + (self.cellH - self._nothing.get_rect().size[1])/2
+                # self.screen.blit(self._nothing, (posx, posy))
+                self.drawItemInCell((location[0], location[1] + 1), percept)
+
+        #         for i in range(len(percept)):
+        #             img = self.getProperpyImage(percept[i])
+        #             posx = self.board[0] + (location[1] + 1)*(self.cellW) + (self.cellW - img.get_rect().size[0])/2
+        #             posy = self.board[1] + location[0]*(self.cellH) + (self.cellH - img.get_rect().size[1])/2
+        #             self.screen.blit(img, (posx, posy))
+        # pg.display.flip()
     def run(self): 
         run = True
         flag = True
+        review = False
         actionListIndex = 0
-        self.screen.blit(self._hud, (0,0))
+        self.screen.blit(self._review, (0,0))
         self.agent.agentLogic()
         # agentPercept, agentLocation, agentDirection, score, agentHP, gold, potion, action 
         actionList = []
         # print (actionList[0][1], actionList[0][2])
         # self.drawAgent(actionList[0][1], actionList[0][2])
         oldPos = (-1, -1)
+        self.drawReviewMap()
         while(run):
             for event in pg.event.get():
                 if event.type == pg.QUIT:
                     run = False
+                if event.type == pg.KEYDOWN:
+                    if event.key == pg.K_k and not review:
+                        self.screen.blit(self._hud, (0,0))
+                        review = True
+                        continue
             if (not run):
                 pg.quit()
-                sys.exit()
-            if actionListIndex >= len(actionList):
-                flag = self.agent.agentLogic()
-                actionList = self.agent.action
-                actionListIndex = 0
-            else:
-                agentPos = actionList[actionListIndex][1]
-                # print(actionList[actionListIndex])
-                
-                print(actionList[actionListIndex][0][agentPos[0]][agentPos[1]])
-                print("===================================")
-                if oldPos !=  (-1,-1):
-                    self.drawAgent(oldPos, (-1, -1))
-                self.drawAgent(actionList[actionListIndex][1], actionList[actionListIndex][2])
-                self.drawPerceptInfo(agentPos, actionList[actionListIndex][0])
-                oldPos = actionList[actionListIndex][1]
-                actionListIndex += 1
+            if (review):
+                if actionListIndex >= len(actionList):
+                    flag = self.agent.agentLogic()
+                    actionList = self.agent.action
+                    actionListIndex = 0
+                else:
+                    agentPos = actionList[actionListIndex][1]
+                    # print(actionList[actionListIndex])
+                    
+                    print(actionList[actionListIndex][0][agentPos[0]][agentPos[1]])
+                    print("===================================")
+                    if oldPos !=  (-1,-1):
+                        self.drawAgent(oldPos, (-1, -1))
+                    self.drawAgent(actionList[actionListIndex][1], actionList[actionListIndex][2])
+                    self.drawPerceptInfo(agentPos, actionList[actionListIndex][0])
+                    self.showStatus(actionList[actionListIndex][3], actionList[actionListIndex][4], 
+                                    actionList[actionListIndex][5], actionList[actionListIndex][6],
+                                    actionList[actionListIndex][7])
+                    oldPos = actionList[actionListIndex][1]
+                    actionListIndex += 1
+                    if not flag:
+                        run = False
             pg.display.flip()
-            pg.time.Clock().tick(8)
-            
+            pg.time.Clock().tick(4)
+                
+def main():
+    filename = input("Path to input map: ")
+    while True:
+        try:
+            sim = GUI(filename)
+            sim.run()
+        except:
+            print("File invalid!!!!")
+        filename = input("PLease input file path for new simulation, or q to quit: ")
+        if (filename.lower() == 'q'):
+            sys.exit()
 
-sim = GUI("map2.txt")
+# main()
+sim = GUI("map1.txt")
 sim.run()
